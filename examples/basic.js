58ed1fe9b43a5cfcdaba9a9c0cfd9c706ec527c3
GenerelSchwerz <xXSG.KingXx@gmail.com>
2024-01-22 09:32:49 -0500

    change basic example

--- examples/basic.js
+++ examples/basic.js
@@ -47,101 +47,145 @@ bot.on("chat", async (username, msg) => {
   const [cmd, ...args] = msg.split(" ");
   const author = bot.nearestEntity((e) => e.username === username);
 
-  if (cmd === "lookat") {
-    bot.lookAt(new Vec3(parseFloat(args[0]), parseFloat(args[1]), parseFloat(args[2])));
-  }
 
-  if (cmd == "placeblock") {
-    await bot.equip(bot.registry.itemsByName.dirt.id, "hand");
-    await bot.placeBlock(bot.blockAtCursor(5), new Vec3(parseInt(args[0]), parseInt(args[1]), parseInt(args[2])));
-  } else if (cmd === "pos") {
-    bot.chat(`I am at ${bot.entity.position}`);
-    console.log(`/tp ${bot.username} ${bot.entity.position.x} ${bot.entity.position.y} ${bot.entity.position.z}`);
-  } else if (cmd === "path") {
-    lastStart = bot.entity.position.clone();
-    const res = bot.pathfinder.getPathTo(new GoalBlock(Number(args[0]), Number(args[1]), Number(args[2])));
-    let test;
-    while ((test = await res.next()).done === false) {
-      console.log(test);
+  switch (cmd) {
+    case "lookat": {
+      bot.lookAt(new Vec3(parseFloat(args[0]), parseFloat(args[1]), parseFloat(args[2])));
+      break;
     }
-  } else if (cmd === "goto" || cmd === "#goto") {
-    const x = Math.floor(Number(args[0]));
-    const y = Math.floor(Number(args[1]));
-    const z = Math.floor(Number(args[2]));
-    if (isNaN(x) || isNaN(y) || isNaN(z)) return bot.chat("goto <x> <y> <z> failed | invalid args");
-    bot.chat(`going to ${args[0]} ${args[1]} ${args[2]}`);
 
-    const startTime = performance.now();
-    await bot.pathfinder.goto(new GoalBlock(x, y, z));
-    const endTime = performance.now();
-    bot.chat(
-      `took ${(endTime - startTime).toFixed(3)} ms, ${Math.ceil((endTime - startTime) / 50)} ticks, ${(
-        (endTime - startTime) /
-        1000
-      ).toFixed(3)} seconds`
-    );
-  } else if (cmd === "pathtome") {
-    if (!author) return bot.chat("failed to find player.");
-    bot.chat("hi");
-    const startTime = performance.now();
-    const res1 = bot.pathfinder.getPathTo(GoalBlock.fromVec(author.position));
-    let test1;
-    let test2 = [];
-    while ((test1 = await res1.next()).done === false) {
-      test2.concat(test1.value.result.path);
+    case "pos": {
+      bot.chat(`I am at ${bot.entity.position}`);
+      console.log(`/tp ${bot.username} ${bot.entity.position.x} ${bot.entity.position.y} ${bot.entity.position.z}`);
+      break;
     }
-    const endTime = performance.now();
-    bot.chat(
-      `took ${(endTime - startTime).toFixed(3)} ms, ${Math.ceil((endTime - startTime) / 50)} ticks, ${(
-        (endTime - startTime) /
-        1000
-      ).toFixed(3)} seconds`
-    );
-    bot.chat(bot.pathfinder.world.getCacheSize());
-    console.log(test2.length);
-  } else if (cmd === "test") {
-    if (!author) return bot.chat("failed to find player.");
-    lastStart = author.position.clone();
-    bot.chat("hi");
-    const startTime = performance.now();
-    await bot.pathfinder.goto(GoalBlock.fromVec(author.position));
-    const endTime = performance.now();
-    bot.chat(
-      `took ${(endTime - startTime).toFixed(3)} ms, ${Math.ceil((endTime - startTime) / 50)} ticks, ${(
-        (endTime - startTime) /
-        1000
-      ).toFixed(3)} seconds`
-    );
-  } else if (cmd === "repath") {
-    if (!lastStart) {
-      bot.chat("no last start");
-      return;
+
+    case "path": {
+      lastStart = bot.entity.position.clone();
+      const res = bot.pathfinder.getPathTo(new GoalBlock(Number(args[0]), Number(args[1]), Number(args[2])));
+      let test;
+      while ((test = await res.next()).done === false) {
+        console.log(test);
+      }
+      break;
     }
-    const res = bot.pathfinder.getPathFromTo(lastStart, bot.entity.velocity, GoalBlock.fromVec(author.position));
-    let test;
-    while ((test = await res.next()).done === false) {
-      console.log(test);
+
+    case "goto":
+    case "#goto": {
+      const x = Math.floor(Number(args[0]));
+      const y = Math.floor(Number(args[1]));
+      const z = Math.floor(Number(args[2]));
+      if (isNaN(x) || isNaN(y) || isNaN(z)) return bot.chat("goto <x> <y> <z> failed | invalid args");
+      bot.chat(`going to ${args[0]} ${args[1]} ${args[2]}`);
+
+      const startTime = performance.now();
+      await bot.pathfinder.goto(new GoalBlock(x, y, z));
+      const endTime = performance.now();
+      bot.chat(
+        `took ${(endTime - startTime).toFixed(3)} ms, ${Math.ceil((endTime - startTime) / 50)} ticks, ${(
+          (endTime - startTime) /
+          1000
+        ).toFixed(3)} seconds`
+      );
+      break;
     }
-  } else if (cmd === "cachesize") {
-    bot.chat(bot.pathfinder.getCacheSize());
-  } else if (cmd == "togglecache" || cmd == "cache") {
-    bot.pathfinder.setCacheEnabled(!bot.pathfinder.isCacheEnabled());
-    bot.chat(`pathfinder cache is now ${bot.pathfinder.isCacheEnabled() ? "enabled" : "disabled"}`);
-  } else if (cmd == "there") {
-    if (!author) return bot.chat("failed to find player");
-    const authorPos = author.position.clone();
-    const rayBlock = rayTraceEntitySight({ entity: author });
-    if (!rayBlock) return bot.chat("No block in sigth");
-    lastStart = rayBlock.position.clone().offset(0.5, 1, 0.5);
-    const startTime = performance.now();
-    await bot.pathfinder.goto(GoalBlock.fromVec(lastStart));
-    const endTime = performance.now();
-    bot.chat(
-      `took ${(endTime - startTime).toFixed(3)} ms, ${Math.ceil((endTime - startTime) / 50)} ticks, ${(
-        (endTime - startTime) /
-        1000
-      ).toFixed(3)} seconds`
-    );
+
+    case "pathtome": {
+      if (!author) return bot.chat("failed to find player.");
+      bot.chat("hi");
+      const startTime = performance.now();
+      const res1 = bot.pathfinder.getPathTo(GoalBlock.fromVec(author.position));
+      let test1;
+      let test2 = [];
+      while ((test1 = await res1.next()).done === false) {
+        test2.concat(test1.value.result.path);
+      }
+      const endTime = performance.now();
+      bot.chat(
+        `took ${(endTime - startTime).toFixed(3)} ms, ${Math.ceil((endTime - startTime) / 50)} ticks, ${(
+          (endTime - startTime) /
+          1000
+        ).toFixed(3)} seconds`
+      );
+      bot.chat(bot.pathfinder.world.getCacheSize());
+      console.log(test2.length);
+      break;
+    }
+
+    case "repath": {
+      if (!lastStart) {
+        bot.chat("no last start");
+        return;
+      }
+      const res = bot.pathfinder.getPathFromTo(lastStart, bot.entity.velocity, GoalBlock.fromVec(author.position));
+      let test;
+      while ((test = await res.next()).done === false) {
+        console.log(test);
+      }
+      break;
+    }
+
+    case "cachesize": {
+      bot.chat(bot.pathfinder.getCacheSize());
+      break;
+    } 
+
+    case "togglecache":
+    case "cache": {
+      bot.pathfinder.setCacheEnabled(!bot.pathfinder.isCacheEnabled());
+      bot.chat(`pathfinder cache is now ${bot.pathfinder.isCacheEnabled() ? "enabled" : "disabled"}`);
+      break;
+    }
+
+    case "there": {
+      if (!author) return bot.chat("failed to find player");
+      const authorPos = author.position.clone();
+      const rayBlock = rayTraceEntitySight({ entity: author });
+      if (!rayBlock) return bot.chat("No block in sigth");
+      lastStart = rayBlock.position.clone().offset(0.5, 1, 0.5);
+      const startTime = performance.now();
+      await bot.pathfinder.goto(GoalBlock.fromVec(lastStart));
+      const endTime = performance.now();
+      bot.chat(
+        `took ${(endTime - startTime).toFixed(3)} ms, ${Math.ceil((endTime - startTime) / 50)} ticks, ${(
+          (endTime - startTime) /
+          1000
+        ).toFixed(3)} seconds`
+      );
+      break;
+    }
+
+    case "test": {
+      if (!author) return bot.chat("failed to find player.");
+      lastStart = author.position.clone();
+      bot.chat("hi");
+      const startTime = performance.now();
+      await bot.pathfinder.goto(GoalBlock.fromVec(author.position));
+      const endTime = performance.now();
+      bot.chat(
+        `took ${(endTime - startTime).toFixed(3)} ms, ${Math.ceil((endTime - startTime) / 50)} ticks, ${(
+          (endTime - startTime) /
+          1000
+        ).toFixed(3)} seconds`
+      );
+      break;
+    }
+
+    case "placeblock": {
+      await bot.equip(bot.registry.itemsByName.dirt.id, "hand");
+      await bot.placeBlock(bot.blockAtCursor(5), new Vec3(parseInt(args[0]), parseInt(args[1]), parseInt(args[2])));
+      break;
+    }
+
+    case "jump": {
+      bot.physics.jump();
+      break;
+    }
+
+    case "stop": {
+      bot.pathfinder.stop();
+      break;
+    }
   }
 });
 
